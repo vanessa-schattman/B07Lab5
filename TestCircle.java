581fb454d24d4479a0717d8a84f74e99d8c260f7
nesssch <v.schattman@gmail.com>
2024-10-23 13:55:12 -0400

    Implements basic test cases for Circle class

--- TestCircle.java
+++ TestCircle.java
@@ -5,4 +5,37 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.Test;
 
-public class TestCircle{}
+public class TestCircle{
+    @Test
+    public void testRadius(){
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(1, 0);
+        Circle c = new Circle(p1, p2);
+        assertEquals(c.Radius(), 1);
+    }
+
+    @Test
+    public void testDiameter(){
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(1, 0);
+        Circle c = new Circle(p1, p2);
+        assertEquals(c.Diameter(), 2);
+    }
+
+     @Test
+    public void testCircumference(){
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(1, 0);
+        Circle c = new Circle(p1, p2);
+        assertEquals(c.Diameter(), 2 * Math.PI);
+    }
+
+     @Test
+    public void testArea(){
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(1, 0);
+        Circle c = new Circle(p1, p2);
+        assertEquals(c.Diameter(), Math.PI);
+    }
+
+}
