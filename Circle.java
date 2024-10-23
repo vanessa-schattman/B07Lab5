982d9d1b75b0f75435753e6b6ec7dd091ec81b01
Jeong Yuseon <jeongyuseon@Yuseon-MacBook-Pro.local>
2024-10-23 14:28:53 -0400

    New circle class

---
+++ Circle.java
@@ -0,0 +1,32 @@
+package lab4;
+ 
+class Circle {
+	Point tangent_point;
+	Point center;
+	
+	
+	Circle(Point center, Point tangent_point) {
+		this.tangent_point = tangent_point;
+		this.center = center;
+	}
+	
+	double Radius() {
+		double Power = Math.pow(tangent_point.y - center.y, 2) + Math.pow(tangent_point.x - center.x, 2);
+		return Math.sqrt(Power);
+	}
+	double Diameter() {
+		double radius = this.Radius();
+		return 2 * radius;
+	}
+	
+	double Circumference() {
+		double radius = this.Radius();
+		return  2 * Math.PI * radius;
+	}
+	double Area() {
+		double radius = this.Radius();
+		return radius * radius * Math.PI;
+	}
+	
+
+}
